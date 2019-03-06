af211961f259d482b089b23251b7c8334459d3d6
andresrallo <andresrallo@gmail.com>
2019-03-06 12:22:33 -0300

    borre las weas

--- CursoAndroid/registro.php
+++
@@ -1,9 +0,0 @@
-<?php include ('functions.php');
-$nombres=$_GET['n'];
-$password=$_GET['p'];
-
-
-ejecutarSQLCommand("INSERT INTO  `usuario` (nombre, pass)
-VALUES ('$nombres','$password')");
-
- ?>
